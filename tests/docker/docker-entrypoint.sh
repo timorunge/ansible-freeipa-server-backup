09fa31ea0ad56a8db34e8424b729236d3747b4ab
Timo Runge <me@timorunge.com>
2018-09-04 16:28:59 +0200

    * Making the docker-entrypoint prettier * Moving configuration for the docker_test_runner to config file

--- tests/docker/docker-entrypoint.sh
+++ tests/docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-printf '[defaults]\nroles_path=/etc/ansible/roles' > ansible.cfg
+printf "[defaults]\nroles_path=/etc/ansible/roles\n" > ansible.cfg
 
 ansible-lint /ansible/test.yml
 ansible-lint /etc/ansible/roles/${ansible_role}/tasks/main.yml
@@ -20,11 +20,11 @@ ansible-playbook /ansible/test.yml \
   -i inventory \
   --connection=local \
   --become | \
-  grep -q 'changed=0.*failed=0' && \
-  (echo 'Idempotence test: pass' && exit 0) || \
-  (echo 'Idempotence test: fail' && exit 1)
+  grep -q "changed=0.*failed=0" && \
+  (echo "Idempotence test: pass" && exit 0) || \
+  (echo "Idempotence test: fail" && exit 1)
 
 expected_crontab_entries=$(crontab -l | grep -e "FreeIPA online backup (data only)" -e "FreeIPA full backup" -e "FreeIPA remove old backups" | wc -l)
-test "3 == ${expected_crontab_entries}" && \
-  (echo 'Crontab test: pass' && exit 0) || \
-  (echo 'Crontab test: fail' && exit 1)
+test "3" = "${expected_crontab_entries}" && \
+  (echo "Crontab test: pass" && exit 0) || \
+  (echo "Crontab test: fail" && exit 1)
